763bc730642101e17c044b4d15147d662b908f47
sh-vasily <vasia.punk.rok@gmail.com>
2022-06-07 09:16:38 +0300

    upload project base

---
+++ Messenger/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Messanger;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private ServiceProvider serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(ServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+}
